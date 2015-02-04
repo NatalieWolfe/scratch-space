305febb4a98d16406b45e0b9a50c31b247be3d9a
Natalie Wolfe <natalie@lifewanted.com>
2015-02-04 12:23:32 -0800

    Changed encryption scheme to be more robust.

---
+++ crypt.py
@@ -0,0 +1,179 @@
+#! /usr/bin/python
+
+from base64 import b64encode, b64decode
+from Crypto import Random
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Random import random
+from Crypto.Signature import PKCS1_v1_5
+
+privateKeyStr = """-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAws5ZlcsFQv8oh+f5YDE/Dqro+tyQxcRpw8Ykjo/Vxq/x7rFg
+CZch7IUWfImTXEiYbePVApgcCFS/yMBJpaG9mWYbYDmpQEMrYEAdo7dB0A6NS/DF
+vdlTmhUxe2YBqeP7U+s5pZ1nekhVD1vCkJroP8Z8pwOZ4kDo1pWDcguL8j0c0a5J
+eO24sBtBxak3lDOlTdIrc6ulJ/cNrhzIhbmuQUTwImsmOH/SYHHKhMctPAU26CRa
+i8NmhIucNx+0LYhikaJXgfdyHD/a7RdSqMHyQWqRjvEyk7DJOEojSEF8OlES24qo
+yMTNRUIndrQc2u96oQToQh9sjg6S0g8TlWc0BwIDAQABAoIBAAYE7D21pTb5vV3d
+rBXtz537Z/GAaTTDKeVztON2zEs0WoxejLIfKlwtjJFSiuwaDiOvG4DWBF+5hqeE
+UYI9qicYQZZKkdE7ghaaRJAvdGgiWvlSujlwgqXLK9k9QKXoNnbUWNamM3FS1NYB
+ptRjBPQbhPSAJvwXt1oSCpq2gp98eBYOIFSXu2CAU9RzMcmf6fnC1ddqR+ZIr7Hy
+J8ud/VByVyW4qth+sUAKNMQaoDGA2laP7LyzHOhGL9B7j/+hhrdkf25onrEdBMf6
+4B1wRZ9Ljfa0UZek874XcaQgX3dBBSjujeECU99iVXKMRQXbP/W2wgrEL+Rt/Lq9
+DzzXVNkCgYEAz4e36LGCKIbGCsc4egCFf7kFcS4UM4Yi5EVDXjUKpJC5JlNMkP5x
+YCmGRrZWrm1texpAj/xjRDkLxusOQrxA/TpEcC3VQMv4iYip6RR1EMKKlYtK71o0
+VQdKFu4Zpe6bvULwmKzGEnzjSKABUbX203ORz06qAbsVyJvY2k+3xiMCgYEA8E3a
+xRC8WWRog+Kf/dIrWlbgwI+oeIOKVdRTMLJOlpevXRSMHf4QwQIE0seQMGkSYXzy
+q8LY6h2Y2mVN7/bWleWP3JhEcI6j3tstz6/Pl1eJOk50Sg5O5NNtDk2DCYr7sqWo
+wGTJZTujqgKaEcwQURcb5hBhO7fOi6stQDzj7s0CgYAJxA03egLx/UWchDt4IkYl
++IL1DIIO2qURsOC2sjBKBjVwREM6H38eU1XqRfJWSvf37whSvkG9mCGhvrXxFgLI
+59EIpl9+qRqM/k1gloHbxAvZdbCVCt2jkrA92/6A/HV/toKz7I11mULoy/7D5Zgz
+4yBdbQo7Ap7Hze2qeE3hmQKBgQCpAVh3eIm/JQsMlnqHfSTYaHmxdFe8SmckbiwD
+96Ol7hB7mlPhVCpcVBsoUwppwbwXrFTP0D3WifJSxyTFPAk3azh+oOihcPB3cBcM
+Fk4d1obA5ySKYP3DmZ79jC44q/LRdgj1Ju0tsYAttZ6+HZLsNSB98c8wvNkbCczN
+fQWhkQKBgFzyQSZzKHKlu3Zfeyg+doPXlXlQVpRHqYUoOkzY83DE5ZujXE5H081P
+bui0fXQMsIFBt+/ItCwZsp9rBXPW0uWtA0jDIEkk5Ro4FuRyx/59/Zllxo3vpZyA
+v7lalsxJIywVo2Ni393HP/63bDKjb99mVzeEfpy7Q89Qh+upP8c6
+-----END RSA PRIVATE KEY-----"""
+
+publicKeyStr = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAws5ZlcsFQv8oh+f5YDE/
+Dqro+tyQxcRpw8Ykjo/Vxq/x7rFgCZch7IUWfImTXEiYbePVApgcCFS/yMBJpaG9
+mWYbYDmpQEMrYEAdo7dB0A6NS/DFvdlTmhUxe2YBqeP7U+s5pZ1nekhVD1vCkJro
+P8Z8pwOZ4kDo1pWDcguL8j0c0a5JeO24sBtBxak3lDOlTdIrc6ulJ/cNrhzIhbmu
+QUTwImsmOH/SYHHKhMctPAU26CRai8NmhIucNx+0LYhikaJXgfdyHD/a7RdSqMHy
+QWqRjvEyk7DJOEojSEF8OlES24qoyMTNRUIndrQc2u96oQToQh9sjg6S0g8TlWc0
+BwIDAQAB
+-----END PUBLIC KEY-----"""
+
+license = """{
+    "some_data": "and its value",
+    "more": "and more",
+    "offline": true,
+    "partner": "bob"
+}"""
+
+# license = """{
+#     "application_id": "uybcrnuTsv7GtPPrc7cElp0E5Qs6yjlP2805-boHHdg=",
+#     "expiration_date": "1970-01-01",
+#     "subscription_type_id": 3,
+#     "offline_access": false,
+#     "secret": "O7zuUaXBIAuxAGgU4eGCRkkEanTYoZkPnl0vCccJExY=",
+#     "user_id": 100183,
+#     "partner_id": "37024de336be4bdba74594e95ff8ad1b",
+#     "start_date": "1970-01-01"
+# }"""
+
+def base64_encode(data):
+    """ Base64 encodes the given data using Pulselocker's URL-safe character set. """
+    return b64encode(data).replace('+', '-').replace('/', '_').replace('\n', '')
+
+
+def base64_decode(data):
+    """ Base64 decodes the given data using Pulselocker's URL-safe character set. """
+    return b64decode(data.replace('-', '+').replace('_', '/'))
+
+
+def encrypt_large_payload(data, public_key, padding="1234567890abcdefghijklmnopqrstuvwxyz"):
+    """
+    Performs alternate encryption scheme. This is best for large messages which are bigger than the
+    key size. First, a random 256-bit key is generated. That key is then used to encrypt the data
+    using AES 256 CBC. The public key is then used to encrypt the key using PKCS1 OAEP. The two are
+    appended together and Base64-encoded for safe transportation.
+
+    Required Paramters:
+    data        The payload to be encrypted.
+    public_key  An RSA public key capable of encrypting 48 bytes (384 bits) of data.
+
+    Optional Parameters:
+    padding     Characters to choose from when padding the message. Defaults to 0-9 and a-z.
+
+    Returns Base64-encoded, AES encrypted message with PKCS1 OAEP encrpyted key.
+    """
+    # Generate the key and IV.
+    random_device = Random.new()
+    aes_key = random_device.read(32) # 256-bit random key
+    aes_iv = random_device.read(16)  # 128-bit random IV
+
+    # Encrypt the key and IV together as the start of the message.
+    rsa_cipher = PKCS1_OAEP.new(RSA.importKey(public_key))
+    encrypted_key_iv = rsa_cipher.encrypt(aes_key + aes_iv)
+
+    # Encrypt the data using AES CBC. CBC requires the data to be an exact multiple of the AES block
+    # size (16 bytes). To satisfy this we will left-pad the payload with a randomly chosen character
+    # from the `padding` parameter to bring it's size to an exact multiple of 16 bytes.
+    pad_char = random.choice(padding)
+    padding_size = (16 - len(data) % 16) - 1 # Minus 1 byte for hex-encoded padding size
+    padded_data = "%x%s" % (padding_size, data.rjust(len(data) + padding_size, pad_char))
+    aes_cipher = AES.new(aes_key, AES.MODE_CBC, aes_iv)
+    encrypted_data = aes_cipher.encrypt(padded_data)
+
+    # Return the whole encrypted key, iv, and data all joined together and Base64 encoded.
+    return base64_encode(encrypted_key_iv + encrypted_data)
+
+
+def decrypt_large_payload(data, private_key):
+    """
+    Reverses the alternate encryption scheme. This method extracts a PKCS1 OAEP encrypted key and IV
+    from the data, then uses that key and IV to perform AES 256 CBC decryption on the rest of the
+    data. From there, it extracts the size of the payload padding, then chops that off and returns
+    the decrypted message with no padding.
+    """
+    # Load the key and find out how big it is.
+    rsa_key = RSA.importKey(private_key)
+    key_size = (rsa_key.size() + 1) / 8
+
+    # Decode the data and separate the key and payload.
+    data = base64_decode(data)
+    encrypted_key = data[:key_size]
+    encrypted_payload = data[key_size:]
+
+    # Decrypt the key, then separate the AES key and IV.
+    rsa_cipher = PKCS1_OAEP.new(rsa_key)
+    decrypted_key = rsa_cipher.decrypt(encrypted_key)
+    aes_key = decrypted_key[:32]
+    aes_iv = decrypted_key[32:]
+
+    # Decrypt the payload and remove the padding.
+    aes_cipher = AES.new(aes_key, AES.MODE_CBC, aes_iv)
+    decrypted_payload = aes_cipher.decrypt(encrypted_payload)
+    padding_size = int(decrypted_payload[0], 16) + 1 # Plus 1 for the padding number itself.
+    payload = decrypted_payload[padding_size:]
+
+    return payload
+
+
+def sign(data, private_key):
+    """
+    Apply SHA-256 hash to (base64 encoded) data and sign it using private_key.
+    Return base64 encoded signature using PKCS#1 v1.5.
+    """
+    rsa_key = RSA.importKey(private_key)
+    signer = PKCS1_v1_5.new(rsa_key)
+    digest = SHA256.new()
+    digest.update(base64_decode(data))  # assume data was encrypted w/ library.
+    signature = signer.sign(digest)
+    return base64_encode(signature)
+
+
+def verify(data, signature, public_key):
+    """
+    Apply SHA-256 hash to (base64 encoded) data and validate it against PKCS#1 v1.5 signature and
+    public_key.
+    Returns Boolean (True if validation passed).
+    """
+    rsa_key = RSA.importKey(public_key)
+    signer = PKCS1_v1_5.new(rsa_key)
+    digest = SHA256.new()
+    digest.update(base64_decode(data))  # assume data was encrypted w/ library.
+    return signer.verify(digest, base64_decode(signature))
+
+
+encrypted = encrypt_large_payload(license, publicKeyStr)
+print "Encrypted payload:", encrypted
+
+print "Decrypted payload:", decrypt_large_payload(encrypted, privateKeyStr)
+
+signature = sign(encrypted, privateKeyStr)
+print "Signature:", signature
+print "Valid:", verify(encrypted, signature, publicKeyStr)
--- decrypt.cpp
+++ decrypt.cpp
@@ -1,223 +1,153 @@
 
-#include <boost/archive/iterators/binary_from_base64.hpp>
-#include <boost/archive/iterators/insert_linebreaks.hpp>
-#include <boost/archive/iterators/remove_whitespace.hpp>
-#include <boost/archive/iterators/transform_width.hpp>
+#include <exception>
 #include <iostream>
+#include <string>
+#include <openssl/aes.h>
+#include <openssl/bio.h>
 #include <openssl/engine.h>
-#include <openssl/err.h>
+#include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/rsa.h>
-#include <stdexcept>
-#include <string>
 
-// ---------------------------------------------------------------------------------------------- //
-
-// static const std::string data =
-//     "nKEjRFsc5igF0stdIqVcb7Ep0QoUwUkFfC2uQ01LoqZ5+rFTIZGBb+WP1EWCTyr2R9WNxzKoTAIJ\n"
-//     "ajGM4MJ+sVGPyrD9wyjWiSBEMGaP9+YgKC/++TlZm1AmwL7t3XoDvXfhtrXFJBC0O8zuh7O3LZmp\n"
-//     "SZ6LQR806xuf9qUj1U4HzfT//tNUPc2ay0fJ+l+YaVUVQ5IcMxO8Oh4f13peVuXeUJRb2XDonuCe\n"
-//     "Ms48moSejCtFpJCWmBoMWk5GkWzGHqFXmS9OYssyVCi6SfkI6lywhwbP1g+OTJy0LI6dvsq+ryJ5\n"
-//     "wcF0xYiNXAdP03OIT2B2DMO2d1BxKBjjSFURiQ==\n"
-// ;
-
-static const std::string data =
-    "hHT9AcaUCPWWI6hARpgskZ2Fg5Wy673byOGcqH3CQFJan5m3+mHDyViOKnXZtB//rIlIFrVb7AH+\n"
-    "munPi3rT9jqxg7BkK+/MdRVEEKiapSzjvBZcEV1j6ixC1pFbzcqCPWf3eA9DDbVuO6ycZ+DHZR7n\n"
-    "vaNZIdqb2qVvUxVh8aSdcJAxVsFYOJFPGMbTGK8uD1b6M3pNnbJvhyJZneuEefzHoSG72eicAfDa\n"
-    "qZd+RrgYoLKL5cGBhcqOKD6jYjfHdu6bcPEo5jX6Au06q3DJUnUOvp2PCjQaRE7w+dj30w9mDRYD\n"
-    "dikUaBaMvjnh3U2LIfmYgMyBjKo03uqWsIkDH7FexA5mkM82QIm2m0yaix5jhUOkTXwpPs+McmbF\n"
-    "Eq0T5qBkeRE/Bbqv+nmWpyJFs/KFaRE5ahFdBQWdDlKmYdTAdSMw2X8AiQZOMctuP6fow7RZ5hkW\n"
-    "/BU96lo89TGnQvhJk1a+MJ1a9X3e4BfJds7FxOACEpLPFXSPkqRO+rJOtjS52+d6y8qGzl1C4N/z\n"
-    "qDTGwTP0GiITcSsJN3XdSpEahZISxgbFtDpuciE1pkwfC76rij/Wtaewx6zqnct2dJEIWOf94WHi\n"
-    "jZCEOxsFojU7djXQV/jJkewpb4IuavW8vWCgwuB1J/pJwBfirwAVD8OSXtrUQFnJCzig0HXCn/mM\n"
-    "FZxB6DeEo2CC7VekClbRnmLrn2YdZkRwHaaYzgcpGPw7MKu4YrBYeQmBQd5fMkwQ4d1X7UgboX5c\n"
-    "zDn+eBxkhNyDu++bbjkFZgPiTtZmXpn4vi41js+lbHqWZiGEIyQoLzkoIqPfQFS57b1QX74rf5df\n"
-    "eskkEHuafJFivGqLVp+SdAuH9XaUpnoo+/B3NuTVAPJ3gUFpzjKNEdreODJiMJrOKdwVwUuGJefp\n"
-    "kxwcjZT3ke7/Kx37oJRVyxijHu29HHqEo68BR2GwOcy2L6+5+JmEqbn4eINJI6SvP9/AafDToZoG\n"
-    "VmVXBwa0VFB9tRciTdfXqriU/lTvqVTxwc9aOaPNMy9pX/jFDqBN9C3C68FvT+MFAcT5bzoMKhMg\n"
-    "EPos6PtvaD+if3x2OPkgy3i2PiXB09U0R8GXt34cK5BFU41UrOYVD32kbvaeNOxeRNFHelglM1bK\n"
-    "aSt8JahlBCS7RS34n4GihuubcpxaPkSKvmdBDJRA7zeB27Vk8Y7cFWfPiE2qRRVTU+C8VM4uLibH\n"
-    "fgoL1jxAgxhR8aul/qNR3zK4N8VyptPLjPa79BRFY5xJ9i+bUo0txsA53DC1wn0B3XlsaviH5G1V\n"
-    "VtykqlA4Z7UzUZusnWFptGsOfb/gw/t5zRBOrmMQr12Ar8LVb6Y42BOjAnIiXzaIOXiPfUJCGHns\n"
-    "NqGl1/2aylpxFZYaArFkukLTmO5anAdgxA7ffvdL3bhkIbK9UGRqZdI9J5eQMC011+kBeMpTQZjx\n"
-    "4RZRVkf4NwW2QU9ZSKFG8hbC7ZP3Vbo8qK99kAtrPxUHyJWJP6qpAMOdk6hRucbZwqH0MWAF2FiL\n"
-    "DPVl5IrYXe7Ceht4aMWEKIXlHzs4RHBZCpj1m9pLbpHTvGA1TusEBwnAxgCeduDBBsVC3+VsG7rA\n"
-    "t9UZGS50rAjp1n+uwPtSDUo6aNhOA0U+iss4OSrLGzTjIoNMvtNgZwrykLA4JXwZPdIpYcuSjnNf\n"
-    "MEtuH/387J7nzn5a+BY+BsvxATMp7ocPtONXVTFLSaWY+mG1gLwctL6FcOtrp+KFygFwudJROnji\n"
-    "rvaMe8jo6kBuAzGcb4ZL7ioYIzWHuxPlFYwYsbgft81dmmUT2ytCDC24ReV5T5r54DfIujZ+enM/\n"
-    "hh16atvU7VvMNlqZQ/MuWqq79LSIlCbNGX5bDvE8YXuwbvTGmp+9A8+OleOgyr/Zr72XKarg7ZvP\n"
-    "2B5yX3JBSOIMf2JZUYoYZ5hvkUzDmSNv9FgIUiZOZZTb77xLpN9y11SQ+8qiRmGadBNcl5E1NgsX\n"
-    "4rx54We7+sNdCl7UpNH7iQbYthBXAk7C/xvurDgXALwjufLxvsQVZOX/KED1T4IIA4OZcgqDJ6lG\n"
-    "TGV8PnfGXaAQtqQ7/y/ikbYgsMkAhZCwguApPm6TrLF5Y/Oh/85VLb7jmcA+FGWIsjBGnNzE3++Q\n"
-    "VyJnLRym8+7g3KI4MlRyDTxubFIQsYCfD0GjazBsEX4LEK/gXndSZ49CE7Byjf1a0x1PXrHwmByt\n"
-    "MUCXjBzVOTAnONarD2Xz7KBGPq2ZmxdsPFTbPqufBk2KW0OaFRviYIrMWDkW45cnQxuJMoVt0BgB\n"
-    "Gs+93dPpEhhQFT3WYjK7OuKKW8Wm/WD6PaPhYo73rzXulR1pt+YY6S081m/33CgVyr3ecr2SshEW\n"
-    "z3wtaoUNWE8jnU/jOfse83QScgo3WWC/PIH1Ww6YWGoVEPdtWwIZNIvg1bZwjLhFq2vdBn2/5x6z\n"
-    "byf3bMZVWZaa7Q96iafRwFYLAgb5yF/vhV7X6/Aj/5QFn234b+p0tSlI9cVcYhxtwkqWONeea2wf\n"
-    "ccryXVNIk3Y/oEIlCSKKpDHUVXN5vk8AtdwKw9f/UxvNkgK/4+WJHRvtGnrsTHjgoqOmqkucBhVc\n"
-    "qnoV6vf7iyHgZFuz+YUcBzQ1+g4HO3fKxvs/KQfJYyNgoSqFl0wb+J3qEM6bQcyfE9LvBzDAdDhh\n"
-    "5I6NfjWL+6lc2nbzsfkoFT8cotWQHslQo1hwjG+qv23qp0YQ/lDXAeAlvXLWRelNDPzrftGWcuP6\n"
-    "xfjWL7gLh5XJV/t7nEZFSgm24noKCLWCNfpOiPB3PuUMff1eKGYkVC9ULyQjmXWrmtTx/1Xb2YHK\n"
-    "aYHl6IUqqsKUN+mE9/4+uofM1ydbhtCE3s9m62HSYaclssZvgW+DcU9zEqzY6s8TD7txdwSNw3GL\n"
-    "xp9hZGuy6eT5D6jZXVcUbZdKyJNHWwJJEEVbk9oVNmktmOYi9aW0PwWloR85IfTG/i6P6WP4uOLR\n"
-    "Qb0Mz99ftAUOPM6Lps5joa94AiArf7rh+Cg7SxemAqZOSXQqjvRFTrd18Y9vFg9RvI3flmuEXMGd\n"
-    "l5toNU/s1jLKdb8eXRpFIPFytsH4PK9rT8KYmXjCBE2jSuYcwxP9Oab/kweo1tbOHQLFHPEGyxCk\n"
-    "tmUDC/KucOcfj+kkTxU/tpvIxVxnA8q3yHfY8K1drMtcUraILtQ5YtWI1fS/SCIGejnl7IpM2Dj8\n"
-    "o5+i5NXg2nLPqYV9/pOElBhLo/s149XAEzTuOYpoScK4FKZPhD1XzlY/EfPs4orwjx8CaHbCf7Lg\n"
-    "zSr6e+IUYNcut0q8JUs0GkdbcdIAgTA5TncqtjRa+qcjaNhDlccsGewl7c9SU2djDYCjK4KYDAn1\n"
-    "xRMeS1OnKWAtl6i8i99dkBCivQvlfZNsAV0cf2zy9sNGdY4DAg/HQ83xU9V54PczlxUP6Lc91pRB\n"
-    "BthxJEYtDqkjZRLdvzuHz/1t9NNH9btc2UAAcjUN95Zguy2myFhAlB0GIR4X6jRwBTlUDt1uReGK\n"
-    "taOvG8mZdc70f2hEJM0eqQUgMaqxoD1QIQRGk5P7H+5Uf90JZrHrR2peJcxlEai35FLKz5h73vbT\n"
-    "Q59vBlL8zbhRX4QlAGuXdu/DmfyX0Ovj3okJubxlfnatrj+x9pt1bx5Dyk76ZQIfGPhLzqS9GOMW\n"
-    "CrWO/ncn/cJ2+IZ/bcNUT6S8oduP7U6uXTYjAFqKkE8hYgWRgasoUYA2po2RhzpHIwhfhNRCfneF\n"
-    "3eTvNtFVFzdArkUUdY1e/2EjQr7dHtc=\n"
-;
-
-// ---------------------------------------------------------------------------------------------- //
-
-// static const char* key =
-//     "-----BEGIN RSA PRIVATE KEY-----\n"
-//     "MIIEogIBAAKCAQEA3A4+JvZx3JaG2iL/Bo5H3aaBW5AtzLTst2JBb2bpJoQmtpON\n"
-//     "DpfDvAOY5TqnPIUtwohVtHzoHgXTDBxkKEtWhfXZ7xuz17jnT/iyLp88iuC1sXZn\n"
-//     "+9tAXbuw/EvZNxV2Z2l2lH6WrgkpT7Y/dhWp0uQ4ygZT/4lg0mPa30WlcEDPfUrO\n"
-//     "FBXfBQHLKAlx+V4zIQd8kznGEOJfpY3PDuBn4fx52qLwDXw/VN3ptcEbpMz/o/FW\n"
-//     "6wwZHdw3qorJHScSq++wP+8rq73LlOPYAEHOtNUARhmAlys+WvEd8e7iUJgeeIM2\n"
-//     "Fb2rFRBSln9+iyYNtvSTZNJFQuXdFsEod7osQQIDAQABAoIBACsuMEv8+ALNFtEf\n"
-//     "JOPob3my/pc1B4evyPODOEvf6chBDketVHSUmzf9jLcOd1qM15ngntC34eRiSDOM\n"
-//     "LqspfWT0BTvCN9wQ0HME3hH/9SVbSsvz7BujvlGmTuhzCcChWYaZT0d8P50O9yhJ\n"
-//     "HtLRFW3okqwx6bGvB2zBY2WEiR4pfFBxaX7FcEP5zf++dbbVvWfadyEyMvpw/mMO\n"
-//     "SasP7sIWa5C/zAzkJ8z+hhdZQHqnKsGjxEGmmOsReZOlbGDQGgfhH4xiJkcwH/9o\n"
-//     "T9eCgVeUNo+Kmt+hvyD9JjqwjNmh3C7cUZU0afGFEjU8vhNxlnnim1Cr6IXkPH0F\n"
-//     "k9LyVAECgYEA4yIFcyhA6VF6G12yzlHXhROb4BrZ+mrzacuIkAFCPmt7PfrjNqKY\n"
-//     "gJDl62Mx+bgD6tOBAnFs8vLkCgOTB4zUDDwJR38utNQdBECiOPemwTTCfPo/lCLX\n"
-//     "AJbXPtR4durr6zazdm0P+gBAKzG1B3PutKkV+Qjd7muvLlf/zXNDKHECgYEA+AX0\n"
-//     "0F31TrIimdy5G8kPUIn1i4Q2TnZRNAoFoUklkiHCLSjb5W/7TQkeO4SoTEnL63jM\n"
-//     "lGMcMQsmln+SjM5scvf/vexu35q+0Gp3Tfolgba+s/P/7+nYhqZQq5mLnAFywNGv\n"
-//     "456vdeFQcCBpkreruVVaUayhvhSrm17sVk8cqNECgYAT/YKNSBWuc0VT80MxdXKK\n"
-//     "e9dUOgjFSXRy8z7prbKu27bVFae5v0W8ywPxW6GiYNe3azYHiQQVvdrkJrxiBbsD\n"
-//     "+KS4bjEmKSUEosukR+edSY90gHHoD8jsfFolZX3UuYhaA054NdEaJ7t+EbnlFgcE\n"
-//     "T31nlagyu7eHVKdm5aYbgQKBgEPubbU8ZtndFHkL6W9gzF8EykoiNjWbVnZoYE5i\n"
-//     "mHXD5Uk1LbVTzIK3Ap7OoMH0XHPsLCxgUPJl2XXTDzKdvzpmDpfYUC3SVn0DDBZB\n"
-//     "c1Bp2yWyqODvLa0DlIjLj191Thbd180CQ5CH2946InBa+h/S7t/75nkHdFEj2Jtz\n"
-//     "I6hhAoGAG7jwHhAQylXq48RJfNoQXOjyFvyZCBOwZgNBoSsRpM7lpyaHjVkDwgFe\n"
-//     "dRiY5bUXxGZVAi1+w66xjg5Vm+jMhZt6RDYALpq22iI6LAdsxw1h6N6jCrIJm/6n\n"
-//     "NrHO5KcXgyzuei+NRI63Vbdi2vgJ9Qu5RdVYsMBcdNIlRWOYc+A=\n"
-//     "-----END RSA PRIVATE KEY-----"
-// ;
-
-static const char* key =
+static const char* privateKey =
     "-----BEGIN RSA PRIVATE KEY-----\n"
-    "MIIEowIBAAKCAQEAyRAyIEc3BwEmhIqa8es+5Vu+0OVdgt/MJl1hUYhqh1OwaFsP\n"
-    "p7PDguZG0u9b/fnAZfI2IXecb72KZnWyZ7f0lPmJefPmes2+0G0ACKt03/ijBAgi\n"
-    "7sxsADbS9zKjOhkYAJZMM7BAYxSr0msmx/AFNXLrVFVY5NTunIuss3lafe7GSmzO\n"
-    "/40pKXMd2DEgRw8V3Jnv52COWrZdBH7kf7RegZc4dG18DY4K7FsN6bFTIv6ekpkZ\n"
-    "7PDqApCJvCYrZO48wSzrfNF7cjQXv5J24rOSvYxHzeTzHcNiJ3mMTC/N4Jp6J0yU\n"
-    "Ws8lPYunOpRHP0UySUqN059nZsGreYIfxxCHlwIDAQABAoIBAC24sB1k9F90ZITp\n"
-    "W/i9TQ0tAsRQFCapq6vhujqnMcpaPphMdM/m8gO3R825voqPm6/TF3oe0AUc7fcE\n"
-    "trAdRdIGuLtxuiyc0tJkpwhRrP8tdCx7iS4ScFDQJsMt0a5pfGrNwsBSSSH+1l/A\n"
-    "HA6FRznzSw5fZhqLTUNuA7TAWNKf7UafRz05GZwwBtFAr5GHM6FKanWVkXOLTqIj\n"
-    "AAFB7PWXQ1ZEvdwjzz6qX6Id3eKd47Qz962FwfFlNZ1ZyprnRkIjTpdQc+9FxdzO\n"
-    "kzQbUGgBDpVg4E2rk+TYDMN8cISWaN4zn9lLeDuhpc+297xHizHhybauS9v9EVq1\n"
-    "FBItd1ECgYEA11fvYAd0gY2Px9AikamfVVPBQ9njkpKYvVYkVIeMjGPEJF3NjQhy\n"
-    "AaM3x3PNcwqqZUtVVmHd0TnkYwwivDQCMZAIxxKpDDCUkEeRvcv3HcpBAhBRXdBZ\n"
-    "agJFb2uBqfmTDb9kaujoSUDpS1jJhPkqag6ka6G/le5rDS42Vt6A+mkCgYEA7wYQ\n"
-    "JqjMSQpdR+Ou1BzL20Eye01fLfS7ANSvAXm6f3LDwdUJ6w3OoHEVJmX9/Ernd0Vz\n"
-    "EssAsQKt2f8XwaHijw73BV0i7vXf0rtQndVMFp8zdDdOq+qGD/sVrjoARFnLdx1C\n"
-    "16VX+6BkScDU653PNYYYC9JDRKRZV/1NNuBBMf8CgYB3irQyrdDKPmiLtooKLIxr\n"
-    "1LPb07AWZNTvfktqtVibcqKmwX/jA8YuDIphaXKqo0gWNSypaGi3xhh3t3vs2D5D\n"
-    "fz6wLDa1sp42PdS09O44lldiKubQWmHYjGw5Pwfa8NXmctHu215fk+4uSpqL+7nE\n"
-    "6F3z9fWh6Jl61qWJsbkNyQKBgBHIlbMKpAhbKQ2MNxm4+sHZW/QokYhNyQwKxuqJ\n"
-    "MAMDeuUVAKPoZxMw1q4AsKDwwb/x/tnLGMYSuPSbMh1J+h1Cs8ZGY677xCddoGXd\n"
-    "1MdqGN3wr33C+S5/M+G+lv5+39CA8NQyjRU/nbaPDkhpeV02GNvoTPDyeWM+iVwn\n"
-    "2zHpAoGBALn4MeHcLz5mQqeZeqqxIOWAPnSBRVEYZ7qf8FURVgFYUWkx+hEQxs9W\n"
-    "YBFU8p8H+DxqnLV+ZodVKBuj5ka7JQXllUV7XJ8MrjB3h0GH4qrp4EXiqnTcr9nI\n"
-    "EaoGKJM29g222yGTdWg9mtwX7NmSwPNEkHJq5tmSrYWa51wMLI/+\n"
+    "MIIEowIBAAKCAQEAws5ZlcsFQv8oh+f5YDE/Dqro+tyQxcRpw8Ykjo/Vxq/x7rFg\n"
+    "CZch7IUWfImTXEiYbePVApgcCFS/yMBJpaG9mWYbYDmpQEMrYEAdo7dB0A6NS/DF\n"
+    "vdlTmhUxe2YBqeP7U+s5pZ1nekhVD1vCkJroP8Z8pwOZ4kDo1pWDcguL8j0c0a5J\n"
+    "eO24sBtBxak3lDOlTdIrc6ulJ/cNrhzIhbmuQUTwImsmOH/SYHHKhMctPAU26CRa\n"
+    "i8NmhIucNx+0LYhikaJXgfdyHD/a7RdSqMHyQWqRjvEyk7DJOEojSEF8OlES24qo\n"
+    "yMTNRUIndrQc2u96oQToQh9sjg6S0g8TlWc0BwIDAQABAoIBAAYE7D21pTb5vV3d\n"
+    "rBXtz537Z/GAaTTDKeVztON2zEs0WoxejLIfKlwtjJFSiuwaDiOvG4DWBF+5hqeE\n"
+    "UYI9qicYQZZKkdE7ghaaRJAvdGgiWvlSujlwgqXLK9k9QKXoNnbUWNamM3FS1NYB\n"
+    "ptRjBPQbhPSAJvwXt1oSCpq2gp98eBYOIFSXu2CAU9RzMcmf6fnC1ddqR+ZIr7Hy\n"
+    "J8ud/VByVyW4qth+sUAKNMQaoDGA2laP7LyzHOhGL9B7j/+hhrdkf25onrEdBMf6\n"
+    "4B1wRZ9Ljfa0UZek874XcaQgX3dBBSjujeECU99iVXKMRQXbP/W2wgrEL+Rt/Lq9\n"
+    "DzzXVNkCgYEAz4e36LGCKIbGCsc4egCFf7kFcS4UM4Yi5EVDXjUKpJC5JlNMkP5x\n"
+    "YCmGRrZWrm1texpAj/xjRDkLxusOQrxA/TpEcC3VQMv4iYip6RR1EMKKlYtK71o0\n"
+    "VQdKFu4Zpe6bvULwmKzGEnzjSKABUbX203ORz06qAbsVyJvY2k+3xiMCgYEA8E3a\n"
+    "xRC8WWRog+Kf/dIrWlbgwI+oeIOKVdRTMLJOlpevXRSMHf4QwQIE0seQMGkSYXzy\n"
+    "q8LY6h2Y2mVN7/bWleWP3JhEcI6j3tstz6/Pl1eJOk50Sg5O5NNtDk2DCYr7sqWo\n"
+    "wGTJZTujqgKaEcwQURcb5hBhO7fOi6stQDzj7s0CgYAJxA03egLx/UWchDt4IkYl\n"
+    "+IL1DIIO2qURsOC2sjBKBjVwREM6H38eU1XqRfJWSvf37whSvkG9mCGhvrXxFgLI\n"
+    "59EIpl9+qRqM/k1gloHbxAvZdbCVCt2jkrA92/6A/HV/toKz7I11mULoy/7D5Zgz\n"
+    "4yBdbQo7Ap7Hze2qeE3hmQKBgQCpAVh3eIm/JQsMlnqHfSTYaHmxdFe8SmckbiwD\n"
+    "96Ol7hB7mlPhVCpcVBsoUwppwbwXrFTP0D3WifJSxyTFPAk3azh+oOihcPB3cBcM\n"
+    "Fk4d1obA5ySKYP3DmZ79jC44q/LRdgj1Ju0tsYAttZ6+HZLsNSB98c8wvNkbCczN\n"
+    "fQWhkQKBgFzyQSZzKHKlu3Zfeyg+doPXlXlQVpRHqYUoOkzY83DE5ZujXE5H081P\n"
+    "bui0fXQMsIFBt+/ItCwZsp9rBXPW0uWtA0jDIEkk5Ro4FuRyx/59/Zllxo3vpZyA\n"
+    "v7lalsxJIywVo2Ni393HP/63bDKjb99mVzeEfpy7Q89Qh+upP8c6\n"
     "-----END RSA PRIVATE KEY-----"
 ;
 
+static const std::string fullMessage =
+    "FHgzBLg5RC1cG2A3USTrXt16wbkP_QfxCdMaxd1OT8ErfU2UxFYTUO6cfXh5vUSeuXS50WOwiJmr8WczDoi01HROg4nqVn"
+    "YVqoHKbk6Ju4UQ_3RJqGbMjSbTRpeOEqlYjaotUpIv55ogu8kzBKJ6ky3abV2iILdAMa-c8yjh6HppGBJnJuTZSmGJIrwm"
+    "r14kVOXosD2N5XCA2jcmkGYRf-6FakUjLz9CQwOoehU8I4OC1cvgbVLcE7GZEEMTcwgfaO_jUsq1xz3H8r3NS0tyLqsRWa"
+    "0dGI7RzjSf4meHm5iIwScnlw96iQf8q0xWXSoATjT4gynS5fy7xqbMLg_BRP0e0WeGQ9AW97Ng0BY8bo35iKqs9DspOOXd"
+    "yXuADY9QD8zlHbEYzhnz_ENqDUHbALP5b5GLXEubmZDXDyPKqlTu4BCa69gilvZCA9L6fKYBtdyMh1q2CmISvqOlP6Mn5m"
+    "5y1TEIltrE0ySbtLggXkw="
+;
+
 // ---------------------------------------------------------------------------------------------- //
 
-RSA* loadKey( const std::string& clientKey ){
-    RSA* privateKey = nullptr;
+std::string base64Decode( const std::string& encoded ){
+    // Prepare our string, reserving an estimate of how large the result will be.
+    std::string decoded;
+    decoded.reserve( encoded.size() * 3 / 4 );
 
-    // Create another buffer for the client key.
-    BIO* keyBuffer = BIO_new_mem_buf( (void*)clientKey.data(), clientKey.size() );
-    if( !keyBuffer ){
-        throw std::runtime_error( "Failed to create private key memory buffer." );
+    // Loop through the string, decoding the characters in batches of 4.
+    char chars[ 4 ] = { 0 };
+    int padding = 0;
+    for( std::size_t i = 0; i < encoded.size(); ++i ){
+        // Don't bother decoding any padding.
+        const char c = encoded[ i ];
+
+        // Decode this character.
+        chars[ i % 4 ] =
+            std::isupper( c ) ? c - 'A' +  0 :
+            std::islower( c ) ? c - 'a' + 26 :
+            std::isdigit( c ) ? c - '0' + 52 :
+            c == '-' ? 62 :
+            c == '_' ? 63 : (++padding, 0)
+        ;
+
+        // Every 4 characters we decode 3 characters and append them to the output.
+        if( i % 4 == 3 ){
+            decoded.append( 1, (chars[ 0 ] << 2) + ((chars[ 1 ] & 0x30) >> 4) );
+            if( padding <= 1 ){
+                decoded.append( 1, ((chars[ 1 ] & 0x0f) << 4) + ((chars[ 2 ] & 0x3c) >> 2) );
+            }
+            if( padding == 0 ){
+                decoded.append( 1, ((chars[ 2 ] & 0x03) << 6) + chars[ 3 ] );
+            }
+        }
     }
 
-    // Convert this memory buffer into a private key.
-    PEM_read_bio_RSAPrivateKey( keyBuffer, &privateKey, nullptr, nullptr );
-    if( !privateKey ){
-        throw std::runtime_error( "Failed to read private key." );
-    }
-
-    // Free up the buffer.
-    BIO_free( keyBuffer );
-    return privateKey;
+    return std::move( decoded );
 }
 
 // ---------------------------------------------------------------------------------------------- //
 
-std::streamsize read( RSA* key, std::string source, char* out ){
-    // Attempt to decrypt the data.
-    std::streamsize bytes = 0;
-    char buffer[ 10240 ] = { 0 };
-    std::size_t keySize = RSA_size( key );
-    std::cout << "Decrypting " << source.size() << " bytes in " << keySize << " byte chunks." << std::endl;
-    for( std::size_t i = 0; i < source.size() - 1; i += keySize ){
-        std::cout << "Decrypting from offset " << i << std::endl;
-
-        std::size_t decryptedSize = RSA_private_decrypt(
-            keySize,
-            (unsigned char*)source.c_str() + i,
-            (unsigned char*)buffer,
-            key,
-            RSA_PKCS1_OAEP_PADDING
-        );
-        bytes += decryptedSize;
-        // std::cout << "Got " << std::string( buffer, decryptedSize ) << std::endl;
-        std::memcpy( out + i, buffer, decryptedSize );
+EVP_PKEY* loadKey( const std::string& priKeyStr ){
+    // Create a buffer.
+    EVP_PKEY* evpKey = nullptr;
+    BIO* buff = BIO_new_mem_buf( (void*)priKeyStr.c_str(), priKeyStr.length() );
+    if( !buff ){
+        throw std::runtime_error( "Failed to load key buffer" );
     }
-    return bytes;
+
+    // Read the data from the buffer.
+    EVP_PKEY* readKey = PEM_read_bio_PrivateKey( buff, &evpKey, nullptr, nullptr );
+    BIO_free( buff );
+    if( readKey == nullptr ){
+        throw std::runtime_error( "Failed to read key from buffer" );
+    }
+
+    return evpKey;
 }
 
 // ---------------------------------------------------------------------------------------------- //
 
-std::string fromBase64( const std::string& b64 ){
-    using namespace boost::archive::iterators;
-    typedef transform_width<
-        binary_from_base64<
-            remove_whitespace< const char* >
-        >,
-        8, 6
-    > Base64Iterator;
-    return std::string(
-        Base64Iterator( &b64.at( 0 ) ),
-        Base64Iterator( &b64.at( b64.size() - 2 ) )
+std::string decryptKey( EVP_PKEY* key, const std::string& encrypted ){
+    RSA* rsaKey = EVP_PKEY_get1_RSA( key );
+    unsigned char buffer[ 1024 ] = { 0 };
+    int size = RSA_private_decrypt(
+        encrypted.size(),
+        (unsigned char*)encrypted.c_str(),
+        buffer,
+        rsaKey,
+        RSA_PKCS1_OAEP_PADDING
     );
+    if( size <= 0 ){
+        throw std::runtime_error( "Failed to decrypt data." );
+    }
+
+    return std::string( (const char*)buffer, size );
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+std::string decryptLicense( const std::string& key, const std::string& message ){
+    unsigned char buffer[ 10240 ] = { 0 };
+    unsigned char iv[ 16 ] = { 0 };
+    std::memcpy( iv, key.c_str() + 32, 16 );
+
+    AES_KEY aesKey = { 0 };
+    AES_set_decrypt_key( (unsigned char*)key.c_str(), 32 * 8, &aesKey );
+    AES_cbc_encrypt((unsigned char*)message.c_str(), buffer, message.size(), &aesKey, iv, AES_DECRYPT );
+
+    return std::string( (const char*)buffer, message.size() );
 }
 
 // ---------------------------------------------------------------------------------------------- //
 
 int main( void ){
-    ERR_load_crypto_strings();
-    std::string decodedData = fromBase64( data );
+    EVP_PKEY* key = loadKey( privateKey );
+    std::string decryptedKey = decryptKey( key, base64Decode( fullMessage ).substr( 0, 256 ) );
+    // std::cout << "Decrypted key: '" << decryptedKey << "'" << std::endl;
 
-    std::cout << "Decoded " << data.size() << " bytes into " << decodedData.size() << " bytes." << std::endl;
-
-    RSA* privateKey = loadKey( key );
-    char decrypted[ 1024 ] = { 0 };
-    int bytes = read( privateKey, decodedData, decrypted );
-    RSA_free( privateKey );
-
-    std::size_t err;
-    while( (err = ERR_get_error()) != 0 ){
-        std::cerr << "Error " << err << ": " << ERR_error_string( err, nullptr ) << std::endl;
-    }
-
-    std::cout << "Decrypted " << bytes << " bytes: '" << (const char*)decrypted << "'." << std::endl;
+    std::string decryptedLicense = decryptLicense( decryptedKey, base64Decode( fullMessage ).substr( 256 ) );
+    std::cout << "Decrypted license: " << decryptedLicense << std::endl;
 
     return 0;
 }
