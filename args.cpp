b50961f8ddf36b645fbd0330e160fb3761a3564c
Natalie Wolfe <natalie@lifewanted.com>
2016-02-16 16:55:53 -0800

    Quoting args

--- args.cpp
+++ args.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, char* argv[]){
     int i = 0;
     for (char** arg = argv; *arg && i < argc; ++arg, ++i) {
-        std::cout << i << " " << *arg << std::endl;
+        std::cout << i << " " << std::quoted(*arg) << std::endl;
     }
 
     return 0;
